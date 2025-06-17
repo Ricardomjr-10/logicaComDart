bbc627e7df83616aa56681c2830f576cd2722a3a
Ricardo-10 <ricardomjr2@gmail.com>
2025-06-17 20:51:18 -0300

    get

--- poo.dart
+++ poo.dart
@@ -1,5 +1,12 @@
 class Carro {
-  final String modelo;
+  final String modelo; //encapsulamento privando a variavel
+  String _segredo = "Muito Dinheiro"; // variavel privada começa com _
+
+  int _valor = 1000;
+
+  //criando get para tornar a variavel privada publica pra consulta
+  int get valorDoCarro => _valor;
+
   Carro(this.modelo);
 }
 
