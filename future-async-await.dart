9378a04f6f4e7dac7150938345d7de27f3a6b80c
Ricardo-10 <ricardomjr2@gmail.com>
2025-06-18 20:40:28 -0300

    Future late

--- future-async-await.dart
+++ future-async-await.dart
@@ -1,11 +1,13 @@
-void main() {
+void main() async {
   //Future, Async e Await
 
   String nome = 'Ricardo';
   Future<String> cepFuture = getCepByName('Rua JK');
   late String cep; // late vai ser iniciado depois
 
-  cepFuture.then((result) => cep = result);
+  //cepFuture.then((result) => cep = result);
+
+  cep = await cepFuture;
 
   print(cep);
 }
