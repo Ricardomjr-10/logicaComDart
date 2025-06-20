441b56e6e10278a6ed4155307cb5dbe130e776b7
Ricardo-10 <ricardomjr2@gmail.com>
2025-06-19 21:20:05 -0300

    callebelInterface

--- callebel-interface.dart
+++ callebel-interface.dart
@@ -2,8 +2,10 @@
 void main() {
   BuscarAlunos buscarAlunos = BuscarAlunos();
   buscarAlunos.buscar();
+  buscarAlunos(); //callebel interface
 }
 
 class BuscarAlunos {
   void buscar() => print('ricardo, rafael, breno');
+  void call() => print('ricardo, rafael, breno'); //callebel interface
 }
