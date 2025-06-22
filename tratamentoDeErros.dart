0eca0a65eca9a15108a7cb079e123b8032cd1e2a
Ricardo-10 <ricardomjr2@gmail.com>
2025-06-21 21:21:20 -0300

    custom error login

--- tratamentoDeErros.dart
+++ tratamentoDeErros.dart
@@ -1,17 +1,39 @@
+// void main() {
+//   try {
+//     // tentar alguma coisa
+//     print((2 / 0).toInt());
+//   } catch (e, stackStrace) {
+//     // capturar a falha
+//     print('printando o erro $e'); // pilha de erros
+//     //rethrow; // propagando o erro
+//     //throw Exception('Ocorreu um erro xpto'); //retornar um novo erro
+//     throw CustomError('error customizado');
+//   }
+// }
+
+// class CustomError implements Exception {
+//   final String error;
+//   CustomError(this.error);
+// }
+
 void main() {
+  Login login = Login();
+
   try {
-    // tentar alguma coisa
-    print((2 / 0).toInt());
-  } catch (e, stackStrace) {
-    // capturar a falha
-    print('printando o erro $e'); // pilha de erros
-    //rethrow; // propagando o erro
-    //throw Exception('Ocorreu um erro xpto'); //retornar um novo erro
-    throw CustomError('error customizado');
+    login.logar();
+  } on PassordLengthError catch (e) {
+    print('Falhou ao logar $e');
   }
 }
 
-class CustomError implements Exception {
-  final String error;
-  CustomError(this.error);
+class Login {
+  void logar() {
+    String user = 'admin';
+    String pass = '123';
+    if (pass.length <= 6) throw PassordLengthError();
+  }
+}
+
+class PassordLengthError implements Exception {
+  PassordLengthError();
 }
