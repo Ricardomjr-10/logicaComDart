dd0b818d5e517289aa323cb384d0c0e5b0d9effd
Ricardo-10 <ricardomjr2@gmail.com>
2025-06-21 21:39:16 -0300

    extensions enums

--- extensions.dart
+++ extensions.dart
@@ -1,5 +1,31 @@
+import 'dart:io';
+
 void main() {
-  String nome = 'ricardo';
+  String nome = 'rocha';
   //primeira letra maiucula
-  print(nome[0].toUpperCase() + nome.substring(1));
+  print(Utils().toFirstToUpperCase(nome));
+  print(nome.toFirstToUpperCase());
+  print('rafael'.toFirstToUpperCase());
+  print(EnumTest.enumValue.toValue());
+}
+
+enum EnumTest { enumValue }
+
+extension ExtensionsEnum on Enum {
+  String toValue() {
+    return 'xpto';
+  }
+}
+
+//criando extension
+extension ExtensionsString on String {
+  String toFirstToUpperCase() {
+    return this[0].toUpperCase() + this.substring(1);
+  }
+}
+
+class Utils {
+  toFirstToUpperCase(String value) {
+    return value[0].toUpperCase() + value.substring(1);
+  }
 }
