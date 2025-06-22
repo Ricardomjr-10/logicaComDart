e139ab314a33cd18a1e58682b01f13837319a3dd
Ricardo-10 <ricardomjr2@gmail.com>
2025-06-22 20:47:12 -0300

    exercicio3 dificil

--- exerciciosIf-esle.dart
+++ exerciciosIf-esle.dart
@@ -1,7 +1,8 @@
 void main() {
   Exercicios exercicios = Exercicios();
   exercicios.exer1(12, 8);
-  exercicios.exer2(40, 60, 80);
+  exercicios.exer2(100, 90, 80);
+  exercicios.exer3(250, 20);
 }
 
 class Exercicios {
@@ -24,6 +25,33 @@ class Exercicios {
       print(n2);
     } else {
       print(n3);
+    }
+  }
+  //dificil
+  //Difícil 11- As organizações CSM resolveram dar um aumento de salário aos seus colaboradores e lhe contrataram para desenvolver o programa que calculará os reajustes.
+
+  // a. Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no salário atual;
+  // b. Salários até R$ 280,00 (incluindo): aumento de 20%;
+  // c. Salários entre R$ 280,00 e R$700,00: aumento de 15%;
+  // d. Salários entre R$ 700,00 e R$ 1500,00: aumento de 10%;
+  // e. Salários de R$ 1500,00 em diante: aumento de 5%
+  // Após o aumento ser realizado; informe na tela;
+
+  // a. O salário antes do reajuste;
+  // b. O percentual de aumento aplicado;
+  // c. O valor do aumento;
+  // d. O novo salário, após o aumento.
+  void exer3(int salario, int reajuste) {
+    if (salario <= 280) {
+      int porcentagem = 20;
+      double reajuste = (salario * porcentagem) / 100;
+      double novoSalario = salario + (salario + reajuste);
+      print(
+        'O salario era de $salario.'
+        'Sofreu um aumento de $porcentagem.'
+        'Valor do aumento foi de $reajuste'
+        'Ficando com novo salario de $novoSalario',
+      );
     }
   }
 }
